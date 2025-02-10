50f51339287cecd7ed46a298f7b58aed0fb177e4
Daniel <danielchenschw@gmail.com>
2025-02-09 23:01:21 -0500

    Update react import

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // We have added this line
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()], // & We have added tailwindcss() here
-});
